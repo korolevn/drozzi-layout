e8ad4257f037c9cb96738a531656599ee1c00a67
nkorolev <orevdrol@mail.ru>
2023-09-28 18:17:30 +0300

    - add main tag

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import { Header } from "./components/Header/Header"
+import { Header } from "./components/Header/Header";
+import { Portfolio } from "./components/Portfolio/Portfolio";
+import { About } from "./components/About/About";
+import { Feedback } from "./components/Feedback/Feedback";
+import { Footer } from "./components/Footer/Footer";
 
 export function App() {
     return (
         <>
         <Header/>
-        <main>
-        </main>
+            <main>
+            <Portfolio/>
+            <About/>
+            <Feedback/>
+            </main>
+        <Footer/>
         </>
     )
 }
